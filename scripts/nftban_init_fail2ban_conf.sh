4214a7d21a0df003b7c0b2c7d155287c68bf5ce5
itcmsgr <contact@itcms.gr>
2025-09-05 23:00:12 +0300

    Update nftban_init_fail2ban_conf.sh

--- scripts/nftban_init_fail2ban_conf.sh
+++ scripts/nftban_init_fail2ban_conf.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+################################################################################
+# Script: nftban_init_fail2ban_conf.sh
+#
+# Version: 1.1
+# Author: ITCMS Team (Antonios Voulvoulis) + Debian/Ubuntu Support
+# Description:
+# Automates fail2ban configuration on Linux (RHEL 8+/Fedora/CentOS/Debian/Ubuntu)
+################################################################################
 
